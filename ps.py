3d4a56f2dbfac997ae534009f0aac4b3bb3a51dd
hp <wonderoalbisi@gmail.com>
2024-04-17 10:35:51 +0100

    fixed alignment issues with navigation menu
    Signed-off-by: hp <wonderoalbisi@gmail.com>

---
+++ ps.py
@@ -0,0 +1,27 @@
+import scapy.all as scapy
+
+def sniff_packets(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_packet)
+
+
+def process_packet(packet):
+    if packet.haslayer(scapy.IP):
+        src_ip = packet[scapy.IP].src
+        dst_ip = packet[scapy.IP].dst
+        protocol = packet[scapy.IP].proto
+        print(f"IP packet: {src_ip} --> {dst_ip} Protocol: {protocol}")
+
+        if packet.haslayer(scapy.TCP):
+            src_port = packet[scapy.TCP].sport
+            dst_port = packet[scapy.TCP].dport
+            print(f"TCP Packet: {src_ip}:{src_port} --> {dst_ip}:{dst_port}")
+
+        elif packet.haslayer(scapy.UDP):
+            src_port = packet[scapy.UDP].sport
+            dst_port = packet[scapy.UDP].dport
+            print(f"UDP Packet:{src_ip}:{src_port} --> {dst_ip}:{dst_port}")
+
+        elif packet.haslayer(scapy.ICMP):
+            icmp_type = packet(scapy.ICMP).type
+            icmp_code = packet[scapy.ICMP].code
+            print(f"ICMP Packet: {src_ip} --> {dst_ip} Type: {icmp_type} Code: {icmp_code}")
